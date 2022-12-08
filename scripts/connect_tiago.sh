b9ab89f5a874a9de467593f36ca940a52fa6ebab
francescodelduchetto <francescodelduchetto@gmail.com>
2022-12-08 16:46:02 +0000

    add check to remote ros enstablished

--- scripts/connect_tiago.sh
+++ scripts/connect_tiago.sh
@@ -11,9 +11,15 @@ ROBOT_IFACE=$2
 
 # Change ROS Master
 export ROS_MASTER_URI=http://tiago-${TIAGO_NUM}c:11311
+rostopic list &>/dev/null
+RETVAL=$?
+if [ $RETVAL -ne 0 ]; then
+    echo "[ERROR] connection with ROS MASTER not enstablished"
+else
+    echo "[OK] Connected to ROS MASTER"
+fi
 
 # Add forwarding address to DNS
-
 ifconfig ${ROBOT_IFACE} | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'
 THIS_IP=$?
 ssh root@tiago-${TIAGO_NUM}c "addLocalDns -u \"${HOSTNAME}\" -i \"${THIS_IP}\""
