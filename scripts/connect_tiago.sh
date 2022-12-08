44042d94ed1a4bae465245cecb3a2d5dff233771
francescodelduchetto <francescodelduchetto@gmail.com>
2022-12-08 16:35:14 +0000

    Adding connection script to TIAGo robot

---
+++ scripts/connect_tiago.sh
@@ -0,0 +1,20 @@
+# Run as `source connect_tiago.sh TIAGO_NUM ROBOT_IFACE`
+# where:
+#   - TIAGO_NUM: is the ID number of the robot you are connected
+#   - ROBOT_IFACE: is the network interface connected to the robot (e.g. wlan0, eth0...)
+
+
+TIAGO_NUM=$1
+ROBOT_IFACE=$2
+#echo $TIAGO_NUM
+#echo $ROBOT_IFACE
+
+# Change ROS Master
+export ROS_MASTER_URI=http://tiago-${TIAGO_NUM}c:11311
+
+# Add forwarding address to DNS
+
+ifconfig ${ROBOT_IFACE} | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'
+THIS_IP=$?
+ssh root@tiago-${TIAGO_NUM}c "addLocalDns -u \"${HOSTNAME}\" -i \"${THIS_IP}\""
+
