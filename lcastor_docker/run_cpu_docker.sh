b407b8cb134ca96ca37506a0ab82a62776e11a0e
francescodelduchetto <francescodelduchetto@gmail.com>
2023-05-10 16:23:45 +0200

    adding script to run in pcs without GPU capabilities

---
+++ lcastor_docker/run_cpu_docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+image_name=lcas.lincoln.ac.uk/lcastor/lcastor_base
+
+xhost + local:docker
+
+echo "Starting docker container..."
+docker run --privileged --network host \
+           --env="DISPLAY=$DISPLAY" \
+           --env="QT_X11_NO_MITSHM=1" \
+           --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+           -v $(pwd)/../:/home/lcastor/ros_ws/src/LCASTOR \
+           -v /dev/dri:/dev/dri \
+           --rm \
+           -it ${image_name}  
+           #--name "${image_name/:/-}" \
+        #    -e ROS_MASTER_URI=${ROS_MASTER_URI} \
+        #    -e ROS_IP=${ROS_IP} \ 
+        #    bash -c "echo ciao & /bin/bash"
