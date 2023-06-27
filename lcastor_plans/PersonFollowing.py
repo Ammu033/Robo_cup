2531dbe212135f5755722af6ee1e3b3e91b61541
Luca Castri <lucacastri94@gmail.com>
2023-06-27 10:56:15 +0100

    plan - action and conditions added

---
+++ lcastor_plans/PersonFollowing.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import rospy
+try:
+    sys.path.insert(0, os.environ["PNP_HOME"] + '/scripts')
+except:
+    print("Please set PNP_HOME environment variable to PetriNetPlans folder.")
+    sys.exit(1)
+
+import time
+import pnp_cmd_ros
+from pnp_cmd_ros import *
+
+SPEAK_TIMEOUT = 5 # [s]
+ROSPARAM = 'lcastor_person_follower/personID_to_follow'
+
+def PersonFollowing(p):
+    rospy.set_param(ROSPARAM, '')
+    while(True):
+        p.exec_action('speak', 'Can_any_of_you_stand_in_front_of_me,_please?')
+        
+        p.exec_action('findClosestPersonToTrack', '')
+        p.exec_action('speak', "Hey_person_" + str(rospy.get_param(ROSPARAM)) + ",_I_am_following_you")
+        
+        p.action_cmd('followPerson', '', 'start')
+        
+        dist_notification = time.time()
+        while not p.get_condition("IsPersonLost"):
+            
+            t = time.time() - dist_notification
+            # time check is needed in order to not say "slow down" 
+            # at each instant the condition isPersonTooFar is True
+            if p.get_condition("IsPersonTooFar") and (t > SPEAK_TIMEOUT):
+                dist_notification = time.time()
+                p.exec_action('speak', 'Can_you_slow_down,_please?')
+                
+            time.sleep(0.1)
+        
+        p.action_cmd('followPerson', '', 'stop')
+        p.exec_action('speak', 'Person_to_follow_lost')
+
+
+if __name__ == "__main__":
+
+    p = PNPCmd()
+
+    p.begin()
+
+    PersonFollowing(p)
+
+    p.end()
