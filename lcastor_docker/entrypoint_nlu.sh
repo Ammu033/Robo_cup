3327286894de950102c05f46b2a1f790a1cd3781
tiago-lcastor <tiagolcastor@gmail.com>
2023-07-01 14:30:39 +0100

    changes left on laptop

--- lcastor_docker/entrypoint_nlu.sh
+++ lcastor_docker/entrypoint_nlu.sh
@@ -6,18 +6,18 @@ set -e
 source "$HOME/.bashrc"
 
 # Cleanup to be "stateless" on startup, otherwise pulseaudio daemon can't start
-rm -rf /var/run/pulse /var/lib/pulse /root/.config/pulse
+#rm -rf /var/run/pulse /var/lib/pulse /root/.config/pulse
 
 # Start pulseaudio as system wide daemon; for debugging it helps to start in non-daemon mode
-pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit
+#pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit
 
 # Create a virtual audio source; fixed by adding source master and format
-echo "Creating virtual audio source: ";
-pactl load-module module-virtual-source master=auto_null.monitor format=s16le source_name=VirtualMic
+#echo "Creating virtual audio source: ";
+#pactl load-module module-virtual-source master=auto_null.monitor format=s16le source_name=VirtualMic
 
 # Set VirtualMic as default input source;
-echo "Setting default source: ";
-pactl set-default-source VirtualMic
+#echo "Setting default source: ";
+#pactl set-default-source VirtualMic
 
 
 echo " "
