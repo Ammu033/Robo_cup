bda2e81a031fa87fa58fbcdecb36487437f7d481
tiago-lcastor <tiagolcastor@gmail.com>
2023-07-01 15:46:29 +0100

    correct name for build

--- lcastor_docker/build_docker_recognition.sh
+++ lcastor_docker/build_docker_recognition.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-image_name=lcas.lincoln.ac.uk/lcastor/lcastor_recognition
+image_name=lcas.lincoln.ac.uk/lcastor/lcastor_base_reco
 
 docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) -t ${image_name} - < Dockerfile_recognition
