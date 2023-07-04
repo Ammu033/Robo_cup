270f7e0f18a7827431acb0e5e9871dd7aa475274
Luca Castri <lucacastri94@gmail.com>
2023-07-04 17:15:49 +0200

    moveTorso added and CarryMyLuggage plan modified

--- lcastor_actions/moveTorso.py
+++ lcastor_actions/moveTorso.py
@@ -11,46 +11,51 @@ import rospy
 from AbstractAction import AbstractAction
 from pnp_msgs.srv import PNPCondition, PNPConditionValue
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+import actionlib
+from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryActionGoal
 
 class moveTorso(AbstractAction):
 
     def _start_action(self):
-        rospy.loginfo('Moving torso to ' + " ".join(self.params))
-        self.torso_cmd = rospy.Publisher(
-			'/torso_controller/command', JointTrajectory, queue_size=1)
-		jt = JointTrajectory()
-		jt.joint_names = ['head_1_joint', 'head_2_joint']
-		jtp = JointTrajectoryPoint()
-		jtp.positions = [float(self.params[0]), float(self.params[1])]
-		jtp.time_from_start = rospy.Duration(2.0)
-		jt.points.append(jtp)
-		self.head_cmd.publish(jt)
-		rospy.loginfo("Done.")
+        
+        rospy.loginfo('Head movement action to position ' + " ".join(self.params) + ' ...')
+
+        if len(self.params) < 1:
+            rospy.logwarn("Wrong use of action, pass vertical position to reach")
+        else:
+            self.ac = actionlib.SimpleActionClient("/torso_controller/follow_joint_trajectory", FollowJointTrajectoryAction)
+            rospy.loginfo("Connecting to /torso_controller/follow_joint_trajectory AS...")
+            self.ac.wait_for_server()
+            rospy.loginfo("Connected.")
+
+            # create goal
+            self.goal = FollowJointTrajectoryActionGoal()
+            # rospy.loginfo(self.goal)
+            self.goal.goal.trajectory.joint_names = ['torso_lift_joint']
+            pt = JointTrajectoryPoint()
+            pt.positions = [float(self.params[0])]
+            pt.time_from_start = rospy.Duration(2.0)
+            self.goal.goal.trajectory.points.append(pt)
+
+            # send goal
+            self.ac.send_goal(self.goal.goal, done_cb=self._on_move_done)
+            rospy.loginfo("Waiting for result...")
+            
+
+    def _on_move_done(self, goalState, result):
+        print("Torso movement DONE", goalState, result)
+
+        self.params.append("done")
 
     def _stop_action(self):
-        rospy.loginfo('Finished moving torso')
+
+        self.ac.cancel_all_goals()
+        rospy.loginfo('STOPPED move torso action')
 
     @classmethod
     def is_goal_reached(cls, params):
-        ''' check conditions CurrentGoal and GoalStartingTime '''
-        condition_value_sp = rospy.ServiceProxy("/PNPConditionValue", PNPConditionValue)
-        condition_eval_sp = rospy.ServiceProxy("/PNPConditionEval", PNPCondition)
+        reached = False
+        if len(params) > 0 and params[-1] == "done":
+            reached = True
 
-        time = 5 #seconds
-        if len(params) > 0:
-            try:
-                time = int(params[0])
-            except ValueError:
-                return False
-
-        # The goal is reached when we have finished saying what we actually want to say
-        current_goal_cond = "CurrentGoal_" + cls.__name__ + "_" + "_".join(params)
-        if condition_eval_sp(current_goal_cond).truth_value:
-            # check that the elapsed time is enough
-            starting_time = condition_value_sp("GoalStartingTime").value
-            if starting_time != "None":
-                elapsed_time = rospy.Time.now() - rospy.Time.from_sec(float(starting_time))
-                if elapsed_time.to_sec() > time:
-                    return True
-
-        return False
+        return reached
