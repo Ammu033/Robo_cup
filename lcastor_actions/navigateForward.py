a606b390be04dd46a58c64e1b30dbe0fe4c2de0c
Riccardo Polvara <pulver22@gmail.com>
2023-07-07 09:59:46 +0200

    Implemented action for navigating forward for a limited amount of time

---
+++ lcastor_actions/navigateForward.py
@@ -0,0 +1,60 @@
+import os
+import sys
+
+try:
+    sys.path.insert(0, os.environ["PNP_HOME"] + '/actions')
+except:
+    print("Please set PNP_HOME environment variable to PetriNetPlans folder.")
+    sys.exit(1)
+
+import rospy
+from AbstractAction import AbstractAction
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+import actionlib
+import math
+from geometry_msgs.msg import Twist
+
+"""
+Starts and stops the object detection node
+"""
+nav_pub = rospy.Publisher("/nav_vel", Twist)
+
+class navigateForward(AbstractAction):
+
+    def _start_action(self):
+        #NOTE: Assume self.params is a list of strings the first element is the name of the node to navigate to
+        rospy.loginfo('Going to ' + " ".join(self.params) + ' ...')
+
+        if len(self.params) < 1:
+            rospy.logwarn("Wrong use of action, pass the coordinates the robots needs to reach in /map frame as X_Y_Theta")
+        else:
+            self.client = actionlib.SimpleActionClient('/move_base', MoveBaseAction)
+            rospy.loginfo("Connecting to /move_base AS...")
+            self.client.wait_for_server()
+            rospy.loginfo("Connected.")
+
+            # Create goal
+            self.nav_msg = Twist()
+            self.nav_msg.linear.x = 0.5
+
+            start_t = rospy.get_time()
+            while rospy.get_time() - start_t <= float(self.params[0]): nav_pub.publish(self.nav_msg)
+            rospy.loginfo("Sending the robot forward...")
+
+    def _on_navigateForward_done(self, goalState, result):
+        print("navigateForward DONE", goalState, result)
+        self.params.append("done")
+        rospy.loginfo('Destination reached')
+
+    def _stop_action(self):
+        self.params.append("done")
+        rospy.loginfo('STOPPED navigateForward action')
+
+    @classmethod
+    def is_goal_reached(cls, params):
+        #TODO also make the necessary changes to make sure this returns True when the navigation has reached the goal
+
+        reached = False
+        if len(params) > 0 and params[-1] == "done":
+            reached = True
+        return reached
