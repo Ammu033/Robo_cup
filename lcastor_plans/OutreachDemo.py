74a1b4730b0f536f8f32f55efa335ea3f076e061
tiago-lcastor <tiagolcastor@gmail.com>
2023-11-22 10:55:03 +0000

    initial plan

---
+++ lcastor_plans/OutreachDemo.py
@@ -0,0 +1,84 @@
+import os
+import sys
+import rospy
+
+try:
+    sys.path.insert(0, os.environ["PNP_HOME"] + '/scripts')
+except:
+    print("Please set PNP_HOME environment variable to PetriNetPlans folder.")
+    sys.exit(1)
+
+import time
+import pnp_cmd_ros
+from pnp_cmd_ros import *
+from wait_for_person import wait_for_person
+from identify_person import identify_person
+from obtain_person_information import obtain_person_information
+from introduce_people import introduce_people
+from do_guest import do_guest
+import time
+import pnp_cmd_ros
+from pnp_cmd_ros import *
+
+def Receptionist(p):
+
+    rospy.set_param('personSaved' , 0)
+
+    rospy.set_param('/host/name' , 'john')
+    rospy.set_param('/host/drink' , 'milk')
+
+#    while not rospy.get_param('modelLoaded'): time.sleep(0.01)
+
+    # p.exec_action('gotoRoom' , 'r_couch1') #TODO PUT BACK
+    time.sleep(2)
+    p.exec_action('moveHead', '0_0.0')
+    p.exec_action('speak' , 'Hello_there,_welcome_to_the_University_of_Lincoln!')
+    p.exec_action('speak', 'I_want_to_know_you_better,can_one_of_you_come_in_front_of_me_please?.')
+    #p.exec_action('speak' , 'Can_you_stand_in_front_of_me_and_look_at_my_eyes,_please?')
+
+    p.exec_action('moveHead', '0_0.3')
+    wait_for_person(p)
+    #p.exec_action('saveGuestData' , 'setloc_host') #TODO Add this after going to couch
+
+    #p.exec_action('personIdentification' , 'learn' )
+    #start_time = rospy.get_time()
+    #saved = rospy.get_param('personSaved')
+    #while not saved: 
+    #    if rospy.get_time() - start_time > 30.:
+    #        break
+    #    saved = rospy.get_param('personSaved')
+    #    time.sleep(1)
+    #if not saved:
+    #    rospy.set_param('LastSavedid' , 15384)
+
+    #p.exec_action('saveGuestData' , 'setid_host')
+    
+
+    #rospy.set_param('personSaved' , 0)
+
+    #p.exec_action('speak' , 'Thank_you,_I_will_wait_at_the_entrance_for_guests!')
+
+
+    # p.exec_action('gotoRoom' , 'r_receptionentrance')
+    
+    do_guest(p, "guest1")
+    
+    #p.exec_action('speak' , 'I_will_now_go_to_the_entrance_and_wait_for_other_guests,_feel_at_home!!!')
+
+    #p.exec_action('gotoRoom' , 'r_receptionentrance')
+
+    #do_guest(p, "guest2")
+
+    #time.sleep(10)
+    #p.exec_action('speak' , 'Lets_have_some_fun!!!!')
+
+
+if __name__ == "__main__":
+
+    p = PNPCmd()
+
+    p.begin()
+
+    Receptionist(p)
+
+    p.end()
--- lcastor_plans/do_guest.py
+++ lcastor_plans/do_guest.py
@@ -20,62 +20,63 @@ def do_guest(p, guest):
 
     wait_for_person(p)
 
-    p.exec_action('speak' , 'Hi,_I_am_tiago,_welcome_to_the_party!')
+    p.exec_action('speak' , 'Hi,_my_name_is_tiago!')
     p.exec_action('speak' , 'Can_you_stand_there_and_look_at_my_eyes,_please?')
     ##
-    detected = False
-    start_time = rospy.get_time()
-    detected = p.get_condition("isPersonDetected")
-    while not detected:
-        if rospy.get_time() - start_time > 10.:
-            p.exec_action('speak' , 'Please_move_a_bit,_so_I_can_see_you_better.')
-            start_time = rospy.get_time()
+    #detected = False
+    #start_time = rospy.get_time()
+    #detected = p.get_condition("isPersonDetected")
+    #while not detected:
+    #    if rospy.get_time() - start_time > 10.:
+    #        p.exec_action('speak' , 'Please_move_a_bit,_so_I_can_see_you_better.')
+    #        start_time = rospy.get_time()#
 
-        detected = p.get_condition("isPersonDetected")
-        time.sleep(1)
+   #     detected = p.get_condition("isPersonDetected")
+    #    time.sleep(1)
 
 
-    p.exec_action('personIdentification' , 'learn' )
+    #p.exec_action('personIdentification' , 'learn' )
 
-    start_time = rospy.get_time()
-    saved = rospy.get_param('personSaved')
-    while not saved: 
-        if rospy.get_time() - start_time > 30.:
-            break
-        time.sleep(1)
-        saved = rospy.get_param('personSaved')
+    #start_time = rospy.get_time()
+    #saved = rospy.get_param('personSaved')
+    #while not saved: 
+    #    if rospy.get_time() - start_time > 30.:
+    #        break
+    #    time.sleep(1)
+    #    saved = rospy.get_param('personSaved')
     
-    if not saved:
-        rospy.set_param('LastSavedid' , 15384)
+    #if not saved:
+    #    rospy.set_param('LastSavedid' , 15384)
 
-    p.exec_action('saveGuestData' , 'setid_{}'.format(guest))
+    #p.exec_action('saveGuestData' , 'setid_{}'.format(guest))
 
     ##
     obtain_person_information(p , guest , 'name')
     obtain_person_information(p , guest , 'drink')
-    p.exec_action('speak' , 'Please_follow_me_to_the_party,_' + rospy.get_param('/{}/name'.format(guest)))
+    p.exec_action('speak', 'Thank_you_' + rospy.get_param('/{}/name'.format(guest)).replace(" ", "_"))
+    p.exec_action('speak', 'Enjoy_the_rest_of_the_day!') 
     
-    if guest == "guest2":
-        p.exec_action('gotoRoom' , 'r_couch2') #TODO PUT BACK 
-    elif guest == "guest1":
-        p.exec_action('gotoRoom' , 'r_couch1') #TODO PUT BACK 
-    p.exec_action('saveGuestData' , 'setloc_host')
-    p.exec_action('saveGuestData' , 'setloc_' + guest)
-    p.exec_action('saveGuestData' , 'setheadangle_' +guest+'_'  + str(0.0))
-    introduce_people(p, 'host' , guest)
+    #if guest == "guest2":
+    #    p.exec_action('gotoRoom' , 'r_couch2') #TODO PUT BACK 
+    #elif guest == "guest1":
+    #    p.exec_action('gotoRoom' , 'r_couch1') #TODO PUT BACK 
+    #p.exec_action('saveGuestData' , 'setloc_host')
+    #p.exec_action('saveGuestData' , 'setloc_' + guest)
+    #p.exec_action('saveGuestData' , 'setheadangle_' +guest+'_'  + str(0.0))
+    #introduce_people(p, 'host' , guest)
 
-    if guest == "guest2":
-        introduce_people(p, 'guest1' , 'guest2')
+    #if guest == "guest2":
+    #    introduce_people(p, 'guest1' , 'guest2')
 
-    if guest == "guest2":
-        p.exec_action('gotoRoom' , 'r_couch2') #TODO PUT BACK 
-    elif guest == "guest1":
-        p.exec_action('gotoRoom' , 'r_couch1') #TODO PUT BACK 
-    p.action_cmd('armAction', 'offer', 'start')
-    p.exec_action('speak' , 'Please_be_seated_on_the_couch_' + str(rospy.get_param('/{}/name'.format(guest))))
-    time.sleep(1)
-    p.action_cmd('armAction', 'offer', 'stop')
-    p.exec_action('armAction', 'home')
+    #if guest == "guest2":
+    #    p.exec_action('gotoRoom' , 'r_couch2') #TODO PUT BACK 
+    #elif guest == "guest1":
+    #    p.exec_action('gotoRoom' , 'r_couch1') #TODO PUT BACK 
+    #p.action_cmd('armAction', 'offer', 'start')
+    #p.exec_action('speak' , 'Please_be_seated_on_the_couch_' + str(rospy.get_param('/{}/name'.format(guest))))
+    #time.sleep(1)
+    #p.action_cmd('armAction', 'offer', 'stop')
+    #p.exec_action('armAction', 'home')
 
 
 if __name__ == "__main__":
