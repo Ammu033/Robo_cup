ef2469b1b47508f0f80b9f308c65ab1193fbdb60
pulver <pulver22@gmail.com>
2024-04-12 14:45:54 +0100

    Added docker file for robot connection from  computer without gpu

---
+++ lcastor_docker/run_robot_cpu_docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+
+image_name=lcas.lincoln.ac.uk/lcastor/lcastor_base
+
+xhost + local:docker
+
+echo "Starting docker container..."
+docker run --privileged --network host \
+           --env="NVIDIA_DRIVER_CAPABILITIES=all" \
+           --env="DISPLAY=$DISPLAY" \
+           --env="QT_X11_NO_MITSHM=1" \
+           --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+           -v $(pwd)/../:/home/lcastor/ros_ws/src/LCASTOR \
+           -v /dev/dri:/dev/dri \
+           --rm \
+           --entrypoint /home/lcastor/ros_ws/src/LCASTOR/lcastor_docker/robot_entrypoint.sh \
+           -it ${image_name}  \
+           $1 $2
+           #--name "${image_name/:/-}" \
+        #    -e ROS_MASTER_URI=${ROS_MASTER_URI} \
+        #    -e ROS_IP=${ROS_IP} \ 
+        #    bash -c "echo ciao & /bin/bash"
