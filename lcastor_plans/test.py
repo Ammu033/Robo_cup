ccdd7c9aff4b300217ac779fa3e0305ead1990bc
Olseda20 <49950899+Olseda20@users.noreply.github.com>
2024-04-26 11:13:03 +0100

    debug configs testing

--- lcastor_plans/test.py
+++ lcastor_plans/test.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 try:
-    sys.path.insert(0, os.environ["PNP_HOME"] + '/scripts')
+    sys.path.insert(0, os.environ["PNP_HOME"] + "/scripts")
 except:
     print("Please set PNP_HOME environment variable to PetriNetPlans folder.")
     sys.exit(1)
@@ -11,31 +11,37 @@ import time
 import pnp_cmd_ros
 from pnp_cmd_ros import *
 
+
 def test(p):
 
     if p.get_condition("JoyPriority_true"):
-        p.exec_action('speak', 'I_can\'t_move._Please_give_priority_to_my_mobile_base!')
+        p.exec_action("speak", "I_can't_move._Please_give_priority_to_my_mobile_base!")
 
         while p.get_condition("JoyPriority_true"):
             time.sleep(1)
 
-        p.exec_action('speak', 'Thank_you!')
+        p.exec_action("speak", "Thank_you!")
 
-    p.action_cmd('speak', 'Looking_left', 'start')
-    p.exec_action('moveHead', '0.6_0')
-    p.action_cmd('speak', 'Looking_left', 'stop')
+    p.action_cmd("speak", "Looking_left", "start")
+    p.exec_action("moveHead", "0.6_0")
+    p.action_cmd("speak", "Looking_left", "stop")
 
-    p.action_cmd('speak', 'Looking_right', 'start')
-    p.exec_action('moveHead', '-0.6_0')
-    p.action_cmd('speak', 'Looking_right', 'stop')
+    p.action_cmd("speak", "Looking_right", "start")
+    p.exec_action("moveHead", "-0.6_0")
+    p.action_cmd("speak", "Looking_right", "stop")
 
-    p.action_cmd('speak', 'Looking_up', 'start')
-    p.exec_action('moveHead', '0_1')
-    p.action_cmd('speak', 'Looking_up', 'stop')
+    p.action_cmd("speak", "Looking_up", "start")
+    p.exec_action("moveHead", "0_1")
+    p.action_cmd("speak", "Looking_up", "stop")
+    #
+    p.action_cmd("speak", "Looking_down", "start")
+    p.exec_action("moveHead", "0_-1")
+    p.action_cmd("speak", "Looking_down", "stop")
 
-    p.action_cmd('speak', 'Looking_down', 'start')
-    p.exec_action('moveHead', '0_-1')
-    p.action_cmd('speak', 'Looking_down', 'stop')
+    p.exec_action("moveHead", "0.0_-0.75")
+    p.exec_action("moveTorso", "0.20")
+    p.exec_action("moveTorso", "0.05")
+
 
 if __name__ == "__main__":
 
