428466cadecf802af2a85ee4b7648e33e31ed8cc
Olseda20 <49950899+Olseda20@users.noreply.github.com>
2024-04-29 11:10:09 +0100

    setting up plan/action/conditions to get the name from a user

---
+++ lcastor_actions/saveGuestDataOllama.py
@@ -0,0 +1,58 @@
+import rospy
+from tf import TransformListener
+from AbstractAction import AbstractAction
+from std_msgs.msg import String
+import json
+
+
+class saveGuestDataOllama(AbstractAction):
+
+    def _start_action(self):
+        self.tf = TransformListener()
+        rospy.loginfo("Starting Reception Action ")
+        # person = rospy.get_param('/person_to_deal')
+        check_param = self.param[0].lower()
+        set_param = self.param[1].lower()
+
+        # if check_param == "setloc":
+        #     p, o = self.get_position()
+        #     rospy.set_param(set_param + "/x", p[0])
+        #     rospy.set_param(set_param + "/y", p[1])
+        #     rospy.set_param(set_param + "/w", o[3])
+        #     rospy.set_param(set_param + "/head_angle", 0.0)
+        # if check_param == "setheadangle":
+        #     rospy.set_param(set_param + "/head_angle", self.params[2])
+        # elif check_param == "setid":
+        #     rospy.set_param(set_param + "/id", rospy.get_param("LastSavedid"))
+        #     rospy.set_param("learn", 0)
+        if check_param == "setname":
+            rospy.set_param(set_param + "/head_angle", 0.0)
+            rospy.set_param(
+                set_param + "/name",
+                rospy.wait_for_message("guest_name", String).data,
+            )
+        # elif check_param == "setdrink":
+        #     rospy.set_param(
+        #         set_param + "/drink",
+        #         rospy.wait_for_message("guest_drink", String).data,
+        #     )
+        # elif(self.params[0] == "savedata"):
+        #    json_obj  = json.dumps(rospy.get_param(self.params[1]) , indent=4)
+        #    with open(self.params[0] + '.json' , "w") as outfile:
+        #    	outfile.write(json_obj)
+        self.params.append("done")
+
+    def get_position(self):
+        now = rospy.Time()
+        self.tf.waitForTransform("/base_link", "/map", now, rospy.Duration(4.0))
+        return self.tf.lookupTransform("/base_link", "/map", now)
+
+    def _stop_action(self):
+        self.params.append("done")
+
+    @classmethod
+    def is_goal_reached(cls, params):
+        reached = False
+        if len(params) > 0 and params[-1] == "done":
+            reached = True
+        return reached
---
+++ lcastor_plans/obtain_person_name.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import rospy
+
+try:
+    sys.path.insert(0, os.environ["PNP_HOME"] + "/scripts")
+except:
+    print("Please set PNP_HOME environment variable to PetriNetPlans folder.")
+    sys.exit(1)
+
+import time
+import pnp_cmd_ros
+from pnp_cmd_ros import *
+from confirm_information_simple import confirm_information_simple
+
+
+def obtain_person_name(p, person, info):
+    person = person.lower()
+    info = info.lower()
+    confirmed = False
+
+    # TODO: while not confirmed starts up the second loop
+    # while not confirmed:
+
+    if info == "name":
+        p.exec_action("speak", "Can_you_please_tell_me_your_name?")
+        p.exec_action("activateOllama", "guest_name")
+
+    start_time = rospy.get_time()
+
+    # waiting on a response confirmation from the user
+    while not p.get_condition("IsOllamaIntentDetected"):
+        # only attempt to ask again if 10 seconds have passed
+        # maybe this should be some kind of timeout parameter
+        if rospy.get_time() - start_time > 10.0:
+            p.exec_action("speak", "Please_repeat_louder,_I_did_not_understand_you.")
+            start_time = rospy.get_time()
+            p.exec_action("activateOllama", "guest_" + info)
+        time.sleep(1)
+
+    time.sleep(2)
+    if info == "name":
+        p.exec_action("saveGuestDataOllama", "setname_" + person)
+
+        # TODO: (from above) confirmed starts up the second loop
+        # confirmed = confirm_information_simple(p, person, info)
+
+
+if __name__ == "__main__":
+
+    p = PNPCmd()
+
+    p.begin()
+
+    obtain_person_name(p, "guest1", "name")
+
+    p.end()
