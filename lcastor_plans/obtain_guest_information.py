dc3e56ca332a84c36f496ddb0323858e901585e8
omro <49950899+Olseda20@users.noreply.github.com>
2024-06-06 17:53:14 +0100

    new obtain_guest_information

---
+++ lcastor_plans/obtain_guest_information.py
@@ -0,0 +1,204 @@
+import os
+import sys
+import rospy
+import time
+from std_msgs.msg import String, Bool
+from ollamamessages.msg import WhisperListening, OllamaResponse
+
+try:
+    sys.path.insert(0, os.environ["PNP_HOME"] + "/scripts")
+except:
+    print("Please set PNP_HOME environment variable to PetriNetPlans folder.")
+    sys.exit(1)
+
+import time
+import pnp_cmd_ros
+from pnp_cmd_ros import *
+from confirm_information_simple import confirm_information_simple
+
+
+def obtain_guest_information(p, person, info):
+    """
+    person: "guest1" or "guest2"
+    info: "name" or "drink"
+    """
+
+    person = person.lower()
+    info = info.lower()
+
+    listening_pub = rospy.Publisher("/stt/listening", WhisperListening, queue_size=1)
+    planner_intent_pub = rospy.Publisher(
+        "/planner_intention", String, queue_size=1, latch=True
+    )
+
+    if info == "name":
+        speech_text = "Please_tell_me_your_name?"
+        incase_name = "Max"
+    elif info == "drink":
+        speech_text = "Please_tell_me_your_favourite_drink?"
+        incase_name = "Milk"
+    else:
+        rospy.logerr("obtain_person_info: invalid info type.")
+        return
+
+    publish_info = f"guest_{info}"
+    p.exec_action("speak", speech_text)
+    planner_intent_pub.publish(publish_info)
+    time.sleep(1)
+    listening_pub.publish(listening=True)
+
+    response = False
+    check = True
+    message_failed = False
+    MAX_TRIES = 2
+    overall_timeout = 120.0
+    ollama_timeout = 10.0
+    ollama_response_timeout = 5.0
+    tries = 0
+
+    initial_start_time = rospy.get_time()
+    start_time = initial_start_time
+
+    while check:
+        if rospy.get_time() - initial_start_time > overall_timeout:
+            p.exec_action(
+                "speak",
+                f"I_did_not_understand_you.I_will_set_your_{info}to_{incase_name}.",
+            )
+            check = False
+            message_failed = True
+            continue
+
+        if (rospy.get_time() - start_time) < ollama_timeout:
+            response = rospy.wait_for_message(
+                "ollama_response", OllamaResponse, timeout=ollama_response_timeout
+            ).data
+
+            # success case
+            if response and response.success:
+                response_intent = response.intent.lower()
+                if response_intent == publish_info:
+                    # TODO: process how response_output is obtained and where
+                    response_output = rospy.wait_for_message(
+                        "ollama_output", String, timeout=ollama_response_timeout
+                    ).data
+                    check = False if response_intent else True
+                    continue
+
+            elif response and not response.success:
+                if tries > MAX_TRIES:
+                    p.exec_action(
+                        "speak",
+                        f"I_did_not_understand_you._Your_{info}_is_{incase_name}.",
+                    )
+                    check = False
+                    message_failed = True
+                    continue
+
+                else:
+                    p.exec_action("speak", "I_did_not_understand_you.")
+                    p.exec_action("speak", speech_text)
+
+                    start_time = rospy.get_time()
+                    planner_intent_pub.publish(publish_info)
+                    tries += 1
+                    continue
+        else:
+            p.exec_action("speak", "Please_repeat_louder.")
+            start_time = rospy.get_time()
+            planner_intent_pub.publish(publish_info)
+            time.sleep(1)
+            continue
+
+    if message_failed:
+        response_output = incase_name
+    else:
+        # confirmation from the user that the information is correct
+        publish_info = f"affirm_deny"
+        speech_text = f"Did_you_say_{publish_info}?"
+        affirm_check = True
+        publish_info = affirm_check
+
+        p.exec_action("speak", speech_text)
+        planner_intent_pub.publish(publish_info)
+        time.sleep(1)
+        listening_pub.publish(listening=True)
+
+        response = False
+        check = True
+        message_failed = False
+        MAX_TRIES = 2
+        overall_timeout = 120.0
+        ollama_timeout = 10.0
+        ollama_response_timeout = 5.0
+        tries = 0
+        response_affirmation = ''
+
+        initial_start_time = rospy.get_time()
+        start_time = initial_start_time
+        while affirm_check:
+            if rospy.get_time() - initial_start_time > overall_timeout:
+                p.exec_action(
+                    "speak",
+                    f"I_did_not_understand_you.I_will_set_your_{info}to_{incase_name}.",
+                )
+                affirm_check = False
+                affirm_failed = True
+                continue
+
+            if (rospy.get_time() - start_time) < ollama_timeout:
+                response = rospy.wait_for_message(
+                    "ollama_response", OllamaResponse, timeout=ollama_response_timeout
+                ).data
+
+                # success case
+                if response and response.success:
+                    response_intent = response.intent.lower()
+                    if response_intent == publish_info:
+                        response_affirmation = rospy.wait_for_message(
+                            "ollama_output", String, timeout=ollama_response_timeout
+                        ).data
+                        affirm_check = False if response_affirmation else True
+                        continue
+
+                    else:
+                        p.exec_action("speak", "I_did_not_understand_you.")
+                        p.exec_action("speak", speech_text)
+
+                        start_time = rospy.get_time()
+                        planner_intent_pub.publish(publish_info)
+                        tries += 1
+            else:
+                p.exec_action("speak", "Please_repeat_louder.")
+                planner_intent_pub.publish(publish_info)
+
+    # TODO: Storing the variable of interest
+    if response_affirmation == "Yes":
+        time.sleep(2)
+        rospy.loginfo(f"obtain_person: saving guest data for {person}...")
+        p.exec_action(
+            "saveGuestDataOllama", f"set{info}_" + person + "_" + response
+        )
+    else:
+        time.sleep(2)
+        rospy.loginfo(f"obtain_person: using an incase value for {person} {info}...")
+        p.exec_action(
+            "saveGuestDataOllama", f"set{info}_" + person + "_" + response
+        )
+    if get_name := rospy.get_param(f"/{person}/name"):
+        p.action_cmd("speak", f"Thank_you_{get_name}", "start")
+    else:
+        p.action_cmd(
+            "speak", f"Thank_you,_{response}_was_stored_as_your_{info}", "start"
+        )
+
+
+if __name__ == "__main__":
+
+    p = PNPCmd()
+
+    p.begin()
+
+    obtain_person_name(p, "guest1", "name")
+
+    p.end()
