23bc2a268c6416ccf4b5175a8dffbda628b02534
tiago-lcastor <tiagolcastor@gmail.com>
2024-06-12 18:08:20 +0100

    Added more benchmarks

--- ollamawrapper/get_dependencies.sh
+++ ollamawrapper/get_dependencies.sh
@@ -5,3 +5,5 @@
 apt-get install -y portaudio19-dev python-all-dev python3-all-dev && pip3 install pyaudio 
 pip3 install jinja2 ollama geocoder requests python-dotenv parsimonious SpeechRecognition
 
+chmod 777 /home/lcastor/ros_ws/src/LCASTOR/ollamawrapper/ollama_benchmarks.csv
+
