37b378bf1fd61f5d85a8fd1d8307b2206afe1103
omro <49950899+Olseda20@users.noreply.github.com>
2024-06-26 16:48:00 +0100

    adding locations for gotoroom

--- lcastor_plans/Receptionist.py
+++ lcastor_plans/Receptionist.py
@@ -22,6 +22,7 @@ from pnp_cmd_ros import *
 
 def Receptionist(p):
     
+    # p.exec_action('gotoRoom' , 'r_door') #TODO PUT BACK
     rospy.set_param('personSaved' , 0)
 
     rospy.set_param('/host/name' , 'john')
@@ -30,7 +31,7 @@ def Receptionist(p):
     while not rospy.get_param('modelLoaded'): time.sleep(0.01)
 
     
-    # p.exec_action('gotoRoom' , 'r_couch1') #TODO PUT BACK
+    p.exec_action('gotoRoom' , 'r_couch1') #TODO PUT BACK
     time.sleep(2)
     p.exec_action('moveHead', '-1_0.3')
     p.exec_action('speak' , 'Hello_John,_I_am_your_receptionist_for_this_party!')
@@ -39,7 +40,7 @@ def Receptionist(p):
 
     p.exec_action('moveHead', '0_0.3')
     wait_for_person(p)
-    #p.exec_action('saveGuestData' , 'setloc_host') #TODO Add this after going to couch
+    p.exec_action('saveGuestData' , 'setloc_host') #TODO Add this after going to couch
 
     p.exec_action('personIdentification' , 'learn' )
     start_time = rospy.get_time()
@@ -58,13 +59,13 @@ def Receptionist(p):
 
     p.exec_action('speak' , 'Thank_you,_I_will_wait_at_the_entrance_for_guests!')
 
-    # p.exec_action('gotoRoom' , 'r_receptionentrance')
+    p.exec_action('gotoRoom' , 'r_door')
     
     do_guest(p, "guest1")
     
     p.exec_action('speak' , 'I_will_now_go_to_the_entrance_and_wait_for_other_guests,_feel_at_home!!!')
 
-    # p.exec_action('gotoRoom' , 'r_receptionentrance')
+    p.exec_action('gotoRoom' , 'r_door')
 
     do_guest(p, "guest2")
 
