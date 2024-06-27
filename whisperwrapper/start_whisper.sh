419c0489e81cd9520baf5facd0edb9b221955ba9
tiago-lcastor <tiagolcastor@gmail.com>
2024-06-27 15:14:31 +0100

    Added script to start whisper and ollama docker, and whisper wrapper ROS node

--- whisperwrapper/start_whisper.sh
+++ whisperwrapper/start_whisper.sh
@@ -1,8 +1,17 @@
-tmux new -d -s whisper
+# to be run on the *HOST* (the dell laptop)
+
+sudo docker compose -f ~/noetic-llama/docker-compose.yaml up -d
+curl http://localhost:11434/api/pull -d '{"name": "llama3"}'
+curl http://localhost:11434/api/pull -d '{"name": "nexusraven:13b-v2-q2_K"}'
+
+tmux new-session -s whisper -d  "cd ~/noetic-llama/whisper_ws && catkin_make && source devel/setup.bash && bash"
 
 tmux split-window -v -t whisper:0
-tmux split-window -v -t whisper:0.1
+tmux split-window -h -t whisper:0.1
 
+sleep 3
 tmux send-keys -t whisper:0.0 C-z 'rosrun whisperwrapper whisperwrapper.py' Enter
-tmux send-keys -t whisper:0.1 C-z 'rostopic echo /stt/listening' Enter
-tmux send-keys -t whisper:0.2 C-z 'rostopic echo /planner_intention' Enter
+tmux send-keys -t whisper:0.2 C-z 'rostopic echo /stt/transcription' Enter
+tmux send-keys -t whisper:0.1 C-z 'echo -e "\n\nTo manually set whisper to listen:\nrostopic pub /stt/listening ollamamessages/WhisperListening \"listening: True\" -1"\n\n' Enter
+sleep 1
+tmux send-keys -t whisper:0.1 C-z 'rostopic pub /stt/listening ollamamessages/WhisperListening "listening: True" -1'
