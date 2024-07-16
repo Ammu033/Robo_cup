598bf80bb901b67ec3d463ca24ed0d0c9ae7ccab
jwansek <eddie.atten.ea29@gmail.com>
2024-07-16 18:45:42 +0000

    Edited the whisper bringup script so hopefully its more consistant

--- whisperwrapper/start_whisper.sh
+++ whisperwrapper/start_whisper.sh
@@ -10,8 +10,9 @@ tmux split-window -v -t whisper:0
 tmux split-window -h -t whisper:0.1
 
 sleep 3
-tmux send-keys -t whisper:0.0 C-z 'rosrun whisperwrapper whisperwrapper.py' Enter
 tmux send-keys -t whisper:0.2 C-z 'rostopic echo /stt/transcription' Enter
 tmux send-keys -t whisper:0.1 C-z 'echo -e "\n\nTo manually set whisper to listen:\nrostopic pub /stt/listening ollamamessages/WhisperListening \"listening: True\" -1"\n\n' Enter
 sleep 1
 tmux send-keys -t whisper:0.1 C-z 'rostopic pub /stt/listening ollamamessages/WhisperListening "listening: True" -1'
+sleep 10
+tmux send-keys -t whisper:0.0 C-z 'rosrun whisperwrapper whisperwrapper.py' Enter
