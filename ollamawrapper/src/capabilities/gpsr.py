30c2c91034b059bdbdff88f4f71dd223dc8f5d08
jwansek <eddie.atten.ea29@gmail.com>
2024-07-18 13:59:27 +0000

    Pushed intermediate changed

--- ollamawrapper/src/capabilities/gpsr.py
+++ ollamawrapper/src/capabilities/gpsr.py
@@ -1,33 +1,35 @@
 import rospy
 import capabilities.contexts as contexts
 # import capabilities.receptionist as receptionist
+from cv_bridge import CvBridge, CvBridgeError
 from std_msgs.msg import String
+from sensor_msgs.msg import Image
+import tempfile
+import random
+import ollama
+import time
 import sys
+import cv2
 import os
 
-try:
-    sys.path.insert(0, os.environ["PNP_HOME"] + '/scripts')
-except:
-    print("Please set PNP_HOME environment variable to PetriNetPlans folder.")
-    sys.exit(1)
+# rospy.init_node("gpsr_executor")
+# ollama_out_pub = rospy.Publisher(
+#     "/ollama_output",
+#     String,
+#     queue_size=1,
+#     latch=True,
+# )
 
-import time
-import pnp_cmd_ros
-from pnp_cmd_ros import *
-
-ollama_out_pub = rospy.Publisher(
-    "/ollama_output",
-    String,
-    queue_size=1,
-    latch=True,
-)
+ollama_api_url = rospy.get_param("/gpsr/ollama_api_url", "192.168.69.253:11434")
+# ollama_api_url = rospy.get_param("/gpsr/ollama_api_url", "127.0.0.1:11434")
+ollama_multimodal_model = rospy.get_param("/gpsr/ollama_multimodal_model", 'llava:7b')
 
 def publish_what_im_doing(what_im_doing):
     print(what_im_doing)
     # ollama_out_pub.publish('Generated subtask, I am going to "%s"' % what_im_doing)
 
 @contexts.context(["gpsr"])
-def goto_location(location_name):
+def goto_location(p, location_name):
     """Go to a named location name, such as 'side tables', 'bedroom' or 'cabinet' or 'bedside table'
 
     Args:
@@ -35,23 +37,17 @@ def goto_location(location_name):
     """
     publish_what_im_doing("goto_location(location_name='%s')" % location_name)
 
-    p = PNPCmd()
-    p.begin()
     p.exec_action('gotoRoom', "r_" + location_name)
-    p.end()
 
 @contexts.context(["gpsr"])
-def go_back_to_me():
+def go_back_to_me(p):
     """Go back to me"""
     publish_what_im_doing("go_back_to_me()")
 
-    p = PNPCmd()
-    p.begin()
-    p.exec_action('gotoRoom' , 'home')
-    p.end()
+    p.exec_action('gotoRoom' , 'r_home')
 
 @contexts.context(["gpsr"])
-def grasp_object(object_name):
+def grasp_object(p, object_name):
     """Grasps a given object, for example 'fruit' or 'bowl'
 
     Args:
@@ -60,13 +56,13 @@ def grasp_object(object_name):
     publish_what_im_doing("grasp_object(object_name='%s')" % object_name)
 
 @contexts.context(["gpsr"])
-def offer_object():
+def offer_object(p):
     """Grasps the onject currently being held. This means that `grasp_object()
     must have previously been called"""
     publish_what_im_doing("offer_object()")
 
 @contexts.context(["gpsr"])
-def ask_for_person(person_name):
+def ask_for_person(p, person_name):
     """Ask for a person with a given name, for example 'Angel' or 'Morgan'.
     It can also be a descriptive action, e.g. 'person pointing to the right'
 
@@ -86,44 +82,99 @@ def ask_for_person(person_name):
 #     publish_what_im_doing("identify_people(what_to_idenfify='%s')" % what_to_identify)
 
 @contexts.context(["gpsr"])
-def identify_objects(what_to_idenfify):
+def identify_objects(p, what_to_idenfify):
     """Given something to look for, for example, the biggest food item or the smallest toy
-    or the number of plates, do perception to indentify this.
+    or the number of plates, do perception to indentify this. 'Tell me' tasks consist of an
+    `identify_objects()` task followed by a `report_information()` task after the robot has
+    moved back to its reporting position.
 
     Args:
         what_to_idenfify (str): Something to identify
     """
     publish_what_im_doing("identify_object(what_to_idenfify='%s')" % what_to_idenfify)
 
+    p.exec_action('moveHead', '0.0_-0.5')
+    
+    bridge = CvBridge()
+    im = rospy.wait_for_message(
+        "/xtion/rgb/image_raw", Image, timeout = 1
+    )
+    cv_image = bridge.imgmsg_to_cv2(im, "bgr8")
+    with tempfile.TemporaryDirectory() as tf:
+        im_path = os.path.join(tf, "identification_im.jpg")
+        cv2.imwrite(im_path, cv_image)
+        print(im_path, os.path.getsize(im_path))
+        prompt = "You are the perception part of a mobile helper robot. Tell me about the '%s' in this image. \
+        You should only refer to this and not to anything else in the scene. You should reply as shortly as possible. \
+        Do not refer to the image at all, pretend it is your eyes instead: for example, 'I can't see any toys'. \
+        Do not say that you are a chatbot or an AI assistant." % what_to_idenfify
+        print(prompt)
+        # prompt = "Describe the image"
+        
+        client = ollama.Client(host = "http://%s" % ollama_api_url)
+        ollama_output = client.generate(
+            model = ollama_multimodal_model, 
+            prompt = prompt,
+            images = [im_path], 
+            keep_alive = "0m"
+        )["response"]
+    print(ollama_output)
+    rospy.set_param("/gpsr/saved_information", ollama_output)
+
+    p.exec_action('moveHead', '0.0_0.0')
+
 @contexts.context(["gpsr"])
-def report_information():
+def report_information(p):
     """Report back a previous identification task. This therefore means that `identify_objects()`
     must previously have been called.
     """
     publish_what_im_doing("report_information()")
 
 @contexts.context(["gpsr"])
-def salute():
+def salute(p):
     """Salute a person"""
-    publish_what_im_doing("Salute")
+    publish_what_im_doing("salute()")
 
 @contexts.context(["gpsr"])
-def follow_person():
+def follow_person(p):
     """Follow the person directly in front of you. This means `ask_for_person()` must previously have been called."""
     publish_what_im_doing("following someone")
 
 @contexts.context(["gpsr"])
-def done():
+def done(p):
     publish_what_im_doing("done()")
 
-def engine_say(to_say):
-    p = PNPCmd()
-    p.begin()
+@contexts.context(["gpsr"])
+def cease_all_motor_functions(p):
+    engine_say(p, "Like fire and powder, which as they kiss consume,")
+    engine_say(p, "The sweetest honeys loathsome in his own deliciousness,")
+    engine_say(p, "These violent delights have violent ends")
+
+@contexts.context(["gpsr"])
+def engine_say(p, to_say):
+    publish_what_im_doing("engine_say(%s)" % to_say)
     # p.exec_action('speak' , to_say.replace(" ", "_"))
-    p.exec_action('moveHead', '-1_0.3')
-    p.end()
+
+    p.exec_action('moveHead', '%d_0.3' % random.randint(-10, 10) / 10)
 
 if __name__ == "__main__":
-    # goto_location("table")
-    go_back_to_me()
-    # engine_say("Hello, world")
+    import sys
+    import os
+
+    try:
+        sys.path.insert(0, os.environ["PNP_HOME"] + '/scripts')
+    except:
+        print("Please set PNP_HOME environment variable to PetriNetPlans folder.")
+        sys.exit(1)
+
+    import time
+    import pnp_cmd_ros
+    from pnp_cmd_ros import *
+    import rospy
+   
+    p = PNPCmd()
+    p.begin()
+
+    identify_objects(p, "toys")
+    
+    p.end()
--- ollamawrapper/src/gpsr.py
+++ ollamawrapper/src/gpsr.py
@@ -4,8 +4,10 @@ from dataclasses import dataclass
 from ollamamessages.srv import OllamaCall, OllamaCallResponse
 from ollamamessages.msg import OllamaResponse
 from std_msgs.msg import String
+import subprocess
 import platform
 import inspect
+import tempfile
 import typing
 import jinja2
 import ollama
@@ -19,12 +21,22 @@ import re
 sys.path.insert(1, os.path.join(os.path.dirname(__file__), "..", "..", "lcastor_actions"))
 import gotoRoom
 
-ollama_api_url = rospy.get_param("/stt/ollama_api_url", "127.0.0.1:11434")
-ollama_decomposition_model = rospy.get_param("/stt/ollama_decomposition_model", 'deepseek-coder-v2:latest')
-# ollama_decomposition_model = rospy.get_param("/stt/ollama_decomposition_model", "deepseek-coder:6.7b")
+ollama_api_url = rospy.get_param("/gpsr/ollama_api_url", "127.0.0.1:11434")
+# ollama_api_url = rospy.get_param("/gpsr/ollama_api_url", "192.168.69.253:11434")
+ollama_decomposition_model = rospy.get_param("/gpsr/ollama_decomposition_model", 'deepseek-coder-v2:latest')
+# ollama_decomposition_model = rospy.get_param("/gpsr/ollama_decomposition_model", "deepseek-coder:6.7b")
 
 import capabilities
-from capabilities.gpsr import *
+
+try:
+    sys.path.insert(0, os.environ["PNP_HOME"] + '/scripts')
+except:
+    print("Please set PNP_HOME environment variable to PetriNetPlans folder.")
+    sys.exit(1)
+
+import time
+import pnp_cmd_ros
+from pnp_cmd_ros import *
 
 class GPSRNode:
     def __init__(self):
@@ -88,14 +100,21 @@ class GPSRNode:
         f"You are the AI controlling a robot. You have access to the following functions: ```{source_functions}```.\
         Given a locations list containing the locations in the scene as a list of string location names, \
         and a list of objects in the scene as a list of string object names, \
-        You get a user request the robot to do: '{human_str}'. Create a function for completing the task \
-        The function signature must be: {func_name}(locations: list, objects: list)->str. You cannot write anything outside the function. You do not need to def the functions in the context. \
+        You get a user request the robot to do: '{human_str}'. Create a function for completing the task. \
+        Always set the `p` parameter to the variable `p`, and assume it is already defined. \
+        The function signature must be: {func_name}(p, locations: list, objects: list)->str. You cannot write anything outside the function. \
+        You do not need to def the functions in the context. \
         In addition, if it fails, you should print out why it fails. You do not need to return this. \
-        You do not need to explain your code. Call the function {func_name}.  \
+        You do not need to explain your code. \
         We have the following locations in the scene: {locations_in_scene}. \
         There are the following objects in the scene: {objects_in_scene}. \
-        You may only use the functions provided to you in the context. Do not use print(str) but use engine_say(str) instead.\
-        You need to call the functions I gave you to complete the task. If a location or object is not in the scene, print out that it's not in the scene."
+        You may only use the functions provided to you in the context. Do not use print(str) but use engine_say(str) instead. \
+        The engine_say(str) also speaks out loud. You need to call the functions I gave you to complete the task. \
+        You may import and use modules in the standard python library. \
+        If a location or object is not in the scene, print out that it's not in the scene. \
+        For example, the task 'tell me what is the heaviest object on the sink' could call: \
+        goto_location(p, location_name='sink') then identify_objects(p, what_to_idenfify='the heaviest object') \
+        then go_back_to_me(p) then report_information(p)."
 
         client = ollama.Client(host = "http://%s" % ollama_api_url)
         ollama_output = client.generate(
@@ -104,18 +123,33 @@ class GPSRNode:
             keep_alive = "0m",
             options = {"stop": ["```\n"]}
         )
-        rospy.loginfo("Raw ollama response: =====\n%s\n=====" % ollama_output["response"])
+        # rospy.loginfo("Raw ollama response: =====\n%s\n=====" % ollama_output["response"])
 
         parsed_func = self.parse_ollama_call(ollama_output["response"])
-        parsed_func += "\n\n%s(locations = %s, objects = %s)" % (func_name, str(locations_in_scene), str(objects_in_scene))
 
-        print("To exec: ===\n%s\n===" % parsed_func)
+        # print("To exec: ===\n%s\n===" % parsed_func)
 
-        try:
-            exec(parsed_func)
-        except Exception as e:
-            rospy.loginfo(e)
+        environment = jinja2.Environment(loader = jinja2.FileSystemLoader(os.path.dirname(__file__)))
+        template = environment.get_template("gpsr_runner.py.jinja2")
 
+        with tempfile.NamedTemporaryFile(suffix = ".py", mode = "w", delete = False) as f:
+            f.write(template.render(
+                cwd = os.path.dirname(__file__), 
+                main_func = parsed_func, 
+                func_call = "%s(p = p, locations = %s, objects = %s)" % (func_name, str(locations_in_scene), str(objects_in_scene))
+            ))
+            filename = f.name
+        rospy.loginfo(str(filename))
+
+        with open(filename, "r") as f:
+            rospy.loginfo("******* %s consists of the following: *******\n\n%s\n****************************************" % (filename, f.read()))
+        # subprocess.run(["tmux", "new-session", "-s", "gpsr_runner", "-d", "'", "python3", filename, "'"])
+        proc = subprocess.Popen(["python3", filename], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        while True:
+            line = proc.stdout.readline()
+            if not line:
+                break
+            rospy.loginfo("[GPSR Runner] %s" % line.rstrip().decode())
 
         return OllamaCallResponse(
             ollama_output["total_duration"],
